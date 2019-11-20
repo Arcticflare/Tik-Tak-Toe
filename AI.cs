17303c1ceee47fc87cbd2f2e37f2c8e143c4e37c
Matt Holt <arcticflare69@gmail.com>
2019-11-20 18:06:53 +1300

    20/11/19

--- AI.cs
+++ AI.cs
@@ -17,109 +17,88 @@ namespace Tik_Tak_Toe
         static Random rand = new Random();
         static List<XO.Square> Squares = new List<XO.Square>();
         static int TURN = 1;
-        static bool CENTER = false;
+
+        private static int TurnPlus(int turn)
+        {
+            return turn++;
+        }
 
         public static void AiTurn(List<XO.Square> squareList)
         {
-            Squares = squareList; //Bring the current Square list into class.
-
-            //Turn 1.
-            if(TURN == 1)
+            switch (TURN)
             {
-                //If player takes center square first.
-                if(Squares[4] == XO.Square.O)
-                {
-                    squareList[8] = XO.Square.X; //Place AI in a corner.
-                    TURN = TURN++;
-                }
-                else
-                {
-                    squareList[4] = XO.Square.X;
-                    CENTER = true;
-                    TURN = TURN++;
-                }
-            } 
-
-            //Turn 2.
-            if(TURN == 2)
-            {
-                //This set is if player has center square, please refactor!!!!!!
-                if (CENTER == true)
-                {
-                    if(Squares[0] == XO.Square.O)
-                    {
-                        squareList[6] = XO.Square.X;
-                    }
-
-                    if(Squares[1] == XO.Square.O)
-                    {
-                        squareList[7] = XO.Square.X;
-                    }
-
-                    if(Squares[2] == XO.Square.O)
-                    {
-                        squareList[7] = XO.Square.X;
-                    }
-
-                    if(Squares[3] == XO.Square.O)
-                    {
-                        squareList[6] = XO.Square.X;
-                    }
-
-                    if(Squares[5] == XO.Square.O)
-                    {
-                        squareList[3] = XO.Square.X;
-                    }
-
-                    if(Squares[6] == XO.Square.O)
-                    {
-                        squareList[2] = XO.Square.X;
-                    }
-
-                    if(Squares[7] == XO.Square.O)
-                    {
-                        squareList[1] = XO.Square.X;
-                    }
-                }
-                else
-                {
-                    if(Squares[0] == XO.Square.O)
-                    {
-                        squareList[6] = XO.Square.X;
-                    }
-
-                    if(Squares[1] == XO.Square.O)
-                    {
-                        squareList[7] = XO.Square.X;
-                    }
-
-                    if(Squares[2] == XO.Square.O)
-                    {
-                        squareList[6] = XO.Square.X;
-                    }
-
-                    if(Squares[3] == XO.Square.O)
-                    {
-                        squareList[5] = XO.Square.O;
-                    }
-
-                    if (Squares[5] == XO.Square.O)
-                    {
-                        squareList[3] = XO.Square.X;
-                    }
-
-                    if (Squares[6] == XO.Square.O)
-                    {
-                        squareList[2] = XO.Square.X;
-                    }
-
-                    if (Squares[7] == XO.Square.O)
-                    {
-                        squareList[1] = XO.Square.X;
-                    }
-                }
-                TURN++;
+                case 1:
+                    AiTurnOne(squareList);
+                    TurnPlus(TURN);
+                    break;
+                case 2:
+                    AiTurnTwo(squareList);
+                    TurnPlus(TURN);
+                    break;
+                default:
+                    break;
             }
         }
+
+
+        public static void AiTurnOne(List<XO.Square> squareList)
+        {
+            Squares = squareList; //Bring the current Square list into class.
+
+            if(Squares[4] == XO.Square.O)
+                squareList[8] = XO.Square.X; //Place AI in a corner.                
+            else
+                squareList[4] = XO.Square.X;
+        }
+
+        private static void AiTurnTwo(List<XO.Square> squareList)
+        {
+            Squares = squareList;
+
+            //Player took center.
+            if(Squares[4] == XO.Square.O)
+            {
+                if (Squares[0] == XO.Square.O)
+                {
+                    squareList[6] = XO.Square.X;
+                    goto FinishTurn;
+                }
+
+                if (Squares[1] == XO.Square.O || Squares[2] == XO.Square.O)
+                {
+                    squareList[7] = XO.Square.X;
+                    goto FinishTurn;
+                }
+
+                if (Squares[3] == XO.Square.O)
+                {
+                    squareList[5] = XO.Square.X;
+                    goto FinishTurn;
+                }
+
+                if (Squares[5] == XO.Square.O)
+                {
+                    squareList[3] = XO.Square.X;
+                    goto FinishTurn;
+                }
+
+                if (Squares[6] == XO.Square.O)
+                {
+                    squareList[2] = XO.Square.X;
+                    goto FinishTurn;
+                }
+
+                if (Squares[7] == XO.Square.O)
+                {
+                    squareList[1] = XO.Square.X;
+                    goto FinishTurn;
+                }
+            }
+
+            //Player took anywhere else.
+            FinishTurn:
+                return;
+        }
     }
+
 }
